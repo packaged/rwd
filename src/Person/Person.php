b36d1c9a966f40de9b092660a094f17ec219577b
Tom Kay <oridan82@gmail.com>
2017-10-31 19:24:34 +0000

    initial person commit

--- src/Person/Person.php
+++ src/Person/Person.php
@@ -1,455 +1,390 @@
 <?php
 namespace Packaged\Rwd\Person;
 
-class Person
+class Person extends AbstractPerson
 {
-  private $_fullName;
+  private $_rawInput;
 
-  protected $prefix = [
-    'Mr.'         => ['Mr.', 'mr', 'mister', 'master'],
-    'Mrs.'        => ['Mrs.', 'mrs', 'missus', 'missis'],
-    'Miss.'       => ['Miss.', 'miss'],
-    'Ms.'         => ['Ms.', 'ms'],
-    'Dr.'         => ['Dr.', 'dr'],
-    'Rev.'        => ['Rev.', 'rev', 'rev\'d', 'reverend'],
-    'Fr.'         => ['Fr.', 'fr', 'father'],
-    'Sr.'         => ['Sr.', 'sr', 'sister'],
-    'Prof.'       => ['Prof.', 'prof', 'professor'],
-    'Sir'         => ['Sir', 'sir'],
-    'Hon.'        => ['Hon.', 'honorable'],
-    'Pres.'       => ['Pres.', 'president'],
-    'Gov'         => ['Gov', 'governor', 'governer'],
-    'Ofc'         => ['Ofc', 'officer'],
-    'Msgr'        => ['Msgr', 'monsignor'],
-    'Br.'         => ['Br.', 'brother'],
-    'Supt.'       => ['Supt.', 'superintendent'],
-    'Rep.'        => ['Rep.', 'representatitve'],
-    'Sen.'        => ['Sen.', 'senator'],
-    'Amb.'        => ['Amb.', 'ambassador'],
-    'Treas.'      => ['Treas.', 'treasurer'],
-    'Sec.'        => ['Sec.', 'secretary'],
-    'Pvt.'        => ['Pvt.', 'private'],
-    'Cpl.'        => ['Cpl.', 'corporal'],
-    'Sgt.'        => ['Sgt.', 'sargent'],
-    'Adm.'        => ['Adm.', 'administrative', 'administrator', 'administrater'],
-    'Maj.'        => ['Maj.', 'major'],
-    'Capt.'       => ['Capt.', 'captain'],
-    'Cmdr.'       => ['Cmdr.', 'commander'],
-    'Lt.'         => ['Lt.', 'lieutenant'],
-    'Lt. Col.'    => ['Lt. Col.', 'lieutenant colonel'],
-    'Col.'        => ['Col.', 'colonel'],
-    'Gen.'        => ['Gen.', 'general'],
-    'Bc.'         => ['Bc.', 'bachelor', 'baccalaureus'],
-    'BcA.'        => ['BcA.', 'bachelor of arts', 'baccalaureus artis'],
-    'ICDr.'       => ['ICDr.', 'doctor of canon law', 'juris cononici doctor'],
-    'Ing.'        => ['Ing.', 'engineer', 'ingenieur'],
-    'Ing. sheet.' => ['Ing. sheet.', 'architect engineer', 'intrudes upon architectus'],
-    'JUDr.'       => ['JUDr.', 'juris doctor utriusque', 'doctor rights'],
-    'MDDr.'       => ['MDDr.', 'doctor of dental medicine', 'medicinae doctor dentium'],
-    'MgA.'        => ['MgA.', 'master of arts', 'magister artis'],
-    'Mgr.'        => ['Mgr.', 'master'],
-    'MD.'         => ['MD.', 'doctor of general medicine'],
-    'DVM.'        => ['DVM.', 'doctor of veterinary medine'],
-    'PaedDr.'     => ['PaedDr.', 'doctor of education'],
-    'PharmDr.'    => ['PharmDr.', 'doctor of pharmacy'],
-    'PhDr.'       => ['PhDr.', 'doctor of philosophy'],
-    'PhMr.'       => ['PhMr.', 'master of pharmacy'],
-    'RCDr.'       => ['RCDr.', 'doctor of business studies'],
-    'RNDr.'       => ['RNDr.', 'doctor of science'],
-    'DSc.'        => ['DSc.', 'doctor of science'],
-    'RSDr.'       => ['RSDr.', 'doctor of socio-political sciences'],
-    'RTDr.'       => ['RTDr.', 'doctor of technical sciences'],
-    'ThDr.'       => ['ThDr.', 'doctor of theology'],
-    'Th.D.'       => ['Th.D.', 'doctor of theology'],
-    'ThLic.'      => ['ThLic.', 'licentiate of theology'],
-    'ThMgr.'      => ['ThMgr.', 'master of theology', 'master of divinity'],
-    'Acad.'       => ['Acad.', 'academian', 'academic'],
-    'ArtD.'       => ['ArtD.', 'doctor of arts'],
-    'DiS.'        => ['DiS.', 'certified specialist'],
-    'As.'         => ['As.', 'assistant'],
-    'Odb. As.'    => ['Odb. As.', 'assistant professor'],
-    'Doc.'        => ['Doc.', 'associate professor'],
-    ' '           => [' ', 'the'],
-  ];
-  protected $compound = [
-    'da',
-    'de',
-    'del',
-    'della',
-    'dem',
-    'den',
-    'der',
-    'di',
-    'du',
-    'het',
-    'la',
-    'onder',
-    'op',
-    'pietro',
-    'st.',
-    'st',
-    '\'t',
-    'ten',
-    'ter',
-    'van',
-    'vanden',
-    'vere',
-    'von',
-  ];
-  protected $suffixLine = [
-    'I',
-    'II',
-    'III',
-    'IV',
-    'V',
-    '1st',
-    '2nd',
-    '3rd',
-    '4th',
-    '5th',
-    'Senior',
-    'Junior',
-    'Jr',
-    'Sr',
-  ];
-  protected $suffixProfession = [
-    'AO',
-    'B.A.',
-    'M.Sc',
-    'BCompt',
-    'PhD',
-    'Ph.D.',
-    'APR',
-    'RPh',
-    'PE',
-    'MD',
-    'M.D.',
-    'MA',
-    'DMD',
-    'CME',
-    'BSc',
-    'Bsc',
-    'BSc(hons)',
-    'Ph.D.',
-    'BEng',
-    'M.B.A.',
-    'MBA',
-    'FAICD',
-    'CM',
-    'OBC',
-    'M.B.',
-    'ChB',
-    'FRCP',
-    'FRSC',
-    'FREng',
-    'Esq',
-    'MEng',
-    'MSc',
-    'J.D.',
-    'JD',
-    'BGDipBus',
-    'Dip',
-    'Dipl.Phys',
-    'M.H.Sc.',
-    'MPA',
-    'B.Comm',
-    'B.Eng',
-    'B.Acc',
-    'FSA',
-    'PGDM',
-    'FCPA',
-    'RN',
-    'R.N.',
-    'MSN',
-    'PCA',
-    'PCCRM',
-    'PCFP',
-    'PCGD',
-    'PCHR',
-    'PCM',
-    'PCPS',
-    'PCPM',
-    'PCSCM',
-    'PCSM',
-    'PCMM',
-    'PCTC',
-    'ACA',
-    'FCA',
-    'ACMA',
-    'FCMA',
-    'AAIA',
-    'FAIA',
-    'CCC',
-    'MIPA',
-    'FIPA',
-    'CIA',
-    'CFE',
-    'CISA',
-    'CFAP',
-    'QC',
-    'Q.C.',
-    'M.Tech',
-    'CTA',
-    'C.I.M.A.',
-    'B.Ec',
-    'CFIA',
-    'ICCP',
-    'CPS',
-    'CAP-OM',
-    'CAPTA',
-    'TNAOAP',
-    'AFA',
-    'AVA',
-    'ASA',
-    'CAIA',
-    'CBA',
-    'CVA',
-    'ICVS',
-    'CIIA',
-    'CMU',
-    'PFM',
-    'PRM',
-    'CFP',
-    'CWM',
-    'CCP',
-    'EA',
-    'CCMT',
-    'CGAP',
-    'CDFM',
-    'CFO',
-    'CGFM',
-    'CGAT',
-    'CGFO',
-    'CMFO',
-    'CPFO',
-    'CPFA',
-    'BMD',
-    'BIET',
-    'P.Eng',
-    'PE',
-    'MBBS',
-    'MB',
-    'BCh',
-    'BAO',
-    'BMBS',
-    'MBBChir',
-    'MBChBa',
-    'MPhil',
-    'LL.D',
-    'LLD',
-    'D.Lit',
-    'DEA',
-    'DESS',
-    'DClinPsy',
-    'DSc',
-    'MRes',
-    'M.Res',
-    'Psy.D',
-    'Pharm.D',
-    'BA(Admin)',
-    'BAcc',
-    'BACom',
-    'BAdmin',
-    'BAE',
-    'BAEcon',
-    'BA(Ed)',
-    'BA(FS)',
-    'BAgr',
-    'BAH',
-    'BAI',
-    'BAI(Elect)',
-    'BAI(Mech)',
-    'BALaw',
-    'BAO',
-    'BAppSc',
-    'BArch',
-    'BArchSc',
-    'BARelSt',
-    'BASc',
-    'BASoc',
-    'DDS',
-    'D.D.S.',
-    'BASS',
-    'BATheol',
-    'BBA',
-    'BBLS',
-    'BBS',
-    'BBus',
-    'BChem',
-    'BCJ',
-    'BCL',
-    'BCLD(SocSc)',
-    'BClinSci',
-    'BCom',
-    'BCombSt',
-    'BCommEdCommDev',
-    'BComp',
-    'BComSc',
-    'BCoun',
-    'BD',
-    'BDes',
-    'BE',
-    'BEcon',
-    'BEcon&Fin',
-    'M.P.P.M.',
-    'MPPM',
-    'BEconSci',
-    'BEd',
-    'BEng',
-    'BES',
-    'BEng(Tech)',
-    'BFA',
-    'BFin',
-    'BFLS',
-    'BFST',
-    'BH',
-    'BHealthSc',
-    'BHSc',
-    'BHy',
-    'BJur',
-    'BL',
-    'BLE',
-    'BLegSc',
-    'BLib',
-    'BLing',
-    'BLitt',
-    'BLittCelt',
-    'BLS',
-    'BMedSc',
-    'BMet',
-    'BMid',
-    'BMin',
-    'BMS',
-    'BMSc',
-    'BMSc',
-    'BMS',
-    'BMus',
-    'BMusEd',
-    'BMusPerf',
-    'BN',
-    'BNS',
-    'BNurs',
-    'BOptom',
-    'BPA',
-    'BPharm',
-    'BPhil',
-    'TTC',
-    'DIP',
-    'Tchg',
-    'BEd',
-    'MEd',
-    'ACIB',
-    'FCIM',
-    'FCIS',
-    'FCS',
-    'Fcs',
-    'Bachelor',
-    'O.C.',
-    'JP',
-    'C.Eng',
-    'C.P.A.',
-    'B.B.S.',
-    'MBE',
-    'GBE',
-    'KBE',
-    'DBE',
-    'CBE',
-    'OBE',
-    'MRICS',
-    'D.P.S.K.',
-    'D.P.P.J.',
-    'DPSK',
-    'DPPJ',
-    'B.B.A.',
-    'GBS',
-    'MIGEM',
-    'M.I.G.E.M.',
-    'FCIS',
-    'BPhil(Ed)',
-    'BPhys',
-    'BPhysio',
-    'BPl',
-    'BRadiog',
-    'BSc',
-    'B.Sc',
-    'BScAgr',
-    'BSc(Dairy)',
-    'BSc(DomSc)',
-    'BScEc',
-    'BScEcon',
-    'BSc(Econ)',
-    'BSc(Eng)',
-    'BScFor',
-    'BSc(HealthSc)',
-    'BSc(Hort)',
-    'BBA',
-    'B.B.A.',
-    'BSc(MCRM)',
-    'BSc(Med)',
-    'BSc(Mid)',
-    'BSc(Min)',
-    'BSc(Psych)',
-    'BSc(Tech)',
-    'BSD',
-    'BSocSc',
-    'BSS',
-    'BStSu',
-    'BTchg',
-    'BTCP',
-    'BTech',
-    'BTechEd',
-    'BTh',
-    'BTheol',
-    'BTS',
-    'EdB',
-    'LittB',
-    'LLB',
-    'MA',
-    'MusB',
-    'ScBTech',
-    'CEng',
-    'FCA',
-    'CFA',
-    'Cfa',
-    'C.F.A.',
-    'LLB',
-    'LL.B',
-    'LLM',
-    'LL.M',
-    'CA(SA)',
-    'C.A.',
-    'CA',
-    'CPA',
-    'Solicitor',
-    'DMS',
-    'FIWO',
-    'CEnv',
-    'MICE',
-    'MIWEM',
-    'B.Com',
-    'BCom',
-    'BAcc',
-    'BA',
-    'BEc',
-    'MEc',
-    'HDip',
-    'B.Bus.',
-    'E.S.C.P.',
-  ];
-  protected $vowels = ['a', 'e', 'i', 'o', 'u'];
+  private $_prefix = [];
+  private $_firstName;
+  private $_middleNames = [];
+  private $_lastNameCompound;
+  private $_lastName;
+  private $_suffix = [];
+  private $_profession = [];
+  private $_nickname;
+
+  /**
+   * @return mixed
+   */
+  public function getRawInput()
+  {
+    return $this->_rawInput;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getPrefix()
+  {
+    return $this->_prefix;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getFirstName()
+  {
+    return $this->_smartUcWords($this->_firstName);
+  }
+
+  /**
+   * @param bool $asArray
+   *
+   * @return array|string
+   */
+  public function getMiddleNames($asArray = false)
+  {
+    return $asArray ? $this->_middleNames : $this->_smartUcWords(implode(' ', $this->_middleNames));
+  }
+
+  /**
+   * @return string
+   */
+  public function getLastNameCompound()
+  {
+    return $this->_smartUcWords($this->_lastNameCompound);
+  }
+
+  /**
+   * @param bool $withCompound
+   *
+   * @return string
+   */
+  public function getLastName($withCompound = true)
+  {
+    $compound = $withCompound ? $this->getLastNameCompound() : '';
+    return ($compound ? $compound . ' ' : '') . $this->_smartUcWords($this->_lastName);
+  }
+
+  private function _smartUcWords($string)
+  {
+    // if whole string is uppercase, lowercase first
+    if($string == strtoupper($string))
+    {
+      $string = strtolower($string);
+    }
+    return ucwords($string);
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getSuffix()
+  {
+    return $this->_suffix;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getProfession()
+  {
+    return $this->_profession;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getNickname()
+  {
+    return $this->_nickname;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getFullName()
+  {
+    return implode(
+      ' ',
+      array_filter(
+        [
+          implode(' ', $this->getPrefix()),
+          $this->getFirstName(),
+          $this->getMiddleNames(),
+          $this->getLastName(),
+          implode(' ', $this->getSuffix()),
+          implode(' ', $this->getProfession()),
+        ]
+      )
+    );
+  }
 
   public function __construct($name)
   {
-    $this->setFullName($name);
-  }
-
-  public function setFullName($name)
-  {
-    $this->_fullName = $name;
+    $this->_rawInput = $name;
+    $this->_parse();
   }
 
   private function _parse()
   {
+    $words = $this->_splitWords($this->_rawInput);
 
+    // suffixes
+    $words = $this->_parseProfession($words);
+    $words = $this->_parseSuffix($words);
+
+    // prefixes
+    $words = $this->_parsePrefix($words);
+
+    // remainder is name
+    $words = $this->_normalizeName($words);
+
+    // nickname
+    $words = $this->_parseNickname($words);
+
+    // name parts
+    $this->_parseName($words);
   }
+
+  private function _splitWords($string)
+  {
+    return array_filter(preg_split('/\s/', $string));
+  }
+
+  private function _normalizeName($words)
+  {
+    // flip if comma
+    $full = implode(' ', $words);
+    if(($pos = strpos($full, ',')) !== false)
+    {
+      $full = trim(substr($full, $pos + 1) . ' ' . substr($full, 0, $pos));
+      $words = explode(' ', $full);
+    }
+    return $words;
+  }
+
+  private function _parseNickname($words)
+  {
+    $full = implode(' ', $words);
+    if(preg_match('/\s("(.*)"|\((.*)\))\s/', $full, $matches))
+    {
+      $full = str_replace($matches[0], ' ', $full);
+      $this->_nickname = $matches[2] ?: $matches[3];
+      return explode(' ', $full);
+    }
+    return $words;
+  }
+
+  private function _parseName($words)
+  {
+    // if we have more than one sr/jr in suffixes, all but last are likely to be part of the name.
+    $found = false;
+    $newWords = [];
+    foreach(array_reverse(array_keys($this->_suffix)) as $suffixKey)
+    {
+      $suffix = $this->_suffix[$suffixKey];
+      if($suffix == 'Senior' || $suffix == 'Junior')
+      {
+        // if only one word and we have Sr or Jr suffix, then it's probably a surname
+        if($found)
+        {
+          unset($this->_suffix[$suffixKey]);
+          array_unshift($newWords, $suffix);
+        }
+        else
+        {
+          $found = true;
+        }
+      }
+    }
+    if(count($newWords) > 0)
+    {
+      $words = array_merge($words, $newWords);
+    }
+
+    if(count($words) == 1)
+    {
+      foreach(array_keys($this->_suffix) as $suffixKey)
+      {
+        $suffix = $this->_suffix[$suffixKey];
+        if($suffix == 'Senior' || $suffix == 'Junior')
+        {
+          unset($this->_suffix[$suffixKey]);
+          array_push($words, $suffix);
+          break;
+        }
+      }
+    }
+
+    $i = 0;
+    foreach($words as $word)
+    {
+      if($this->_isCompoundFragment($word))
+      {
+        $this->_lastNameCompound = $word;
+        $this->_lastName = implode(' ', array_slice($words, $i + 1));
+        $words = array_slice($words, 0, $i);
+        break;
+      }
+      $i++;
+    }
+
+    $this->_firstName = array_shift($words);
+
+    if(count($words) > 0 && empty($this->_lastName))
+    {
+      $this->_lastName = array_pop($words);
+    }
+
+    if(count($words) > 0)
+    {
+      $this->_middleNames = $words;
+    }
+
+    return [];
+  }
+
+  private function _parsePrefix($words)
+  {
+    $this->_prefix = [];
+
+    while(true)
+    {
+      if(count($words) <= 0)
+      {
+        break;
+      }
+
+      $word = reset($words);
+
+      if(($replace = $this->_getPrefixWord($word)) !== null)
+      {
+        array_push($this->_prefix, $replace);
+        array_shift($words);
+      }
+      else if(strrpos($word, '.') === false)
+      {
+        break;
+      }
+      else
+      {
+        $subWords = explode('.', $word);
+        $subCount = count($subWords);
+        $i = 0;
+        while($i < $subCount)
+        {
+          $subWord = implode('.', array_slice($subWords, $i, 1));
+          if(($replace = $this->_getPrefixWord($subWord)) !== null)
+          {
+            array_push($this->_prefix, $replace);
+            // shift word
+            array_shift($words);
+            // split first part off and unshift the rest
+            array_unshift($words, implode('.', array_slice($subWords, $i + 1)));
+            break;
+          }
+          $i++;
+        }
+      }
+    }
+
+    return $words;
+  }
+
+  private function _parseSuffix($words)
+  {
+    $this->_suffix = [];
+
+    while(true)
+    {
+      if(count($words) <= 0)
+      {
+        break;
+      }
+
+      $word = end($words);
+
+      if(($replace = $this->_getNameSuffixWord($word)) !== null)
+      {
+        array_unshift($this->_suffix, $replace);
+        array_pop($words);
+      }
+      else if(strrpos($word, '.') === false)
+      {
+        break;
+      }
+      else
+      {
+        $subWords = explode('.', $word);
+        $subCount = count($subWords);
+        $i = 1;
+        while($i < $subCount)
+        {
+          $subWord = implode('.', array_slice($subWords, $subCount - $i));
+          if(($replace = $this->_getNameSuffixWord($subWord)) !== null)
+          {
+            array_unshift($this->_suffix, $replace);
+            // shift word
+            array_pop($words);
+            // split last part off and unshift the rest
+            array_push($words, implode('.', array_slice($subWords, 0, $subCount - $i)));
+            break;
+          }
+          $i++;
+        }
+      }
+    }
+
+    return $words;
+  }
+
+  private function _parseProfession($words)
+  {
+    $this->_profession = [];
+
+    while(true)
+    {
+      if(count($words) <= 0)
+      {
+        break;
+      }
+
+      $word = end($words);
+
+      if(($replace = $this->_getProfessionSuffixWord($word)) !== null)
+      {
+        array_unshift($this->_profession, $replace);
+        array_pop($words);
+      }
+      else if(strrpos($word, '.') === false)
+      {
+        break;
+      }
+      else
+      {
+        $subWords = explode('.', $word);
+        $subCount = count($subWords);
+        $i = 1;
+        while($i < $subCount)
+        {
+          $subWord = implode('.', array_slice($subWords, $subCount - $i));
+          if(($replace = $this->_getProfessionSuffixWord($subWord)) !== null)
+          {
+            array_unshift($this->_profession, $replace);
+            // shift word
+            array_pop($words);
+            // split last part off and unshift the rest
+            array_push($words, implode('.', array_slice($subWords, 0, $subCount - $i)));
+            break;
+          }
+          $i++;
+        }
+      }
+    }
+
+    return $words;
+  }
+
 }
---
+++ tests/Person/PersonTest.php
@@ -0,0 +1,134 @@
+<?php
+namespace Packaged\Tests\Rwd\Person;
+
+use Packaged\Rwd\Person\Person;
+
+class PersonTest extends \PHPUnit_Framework_TestCase
+{
+  /**
+   * @dataProvider data
+   *
+   * @param string $name
+   * @param array  $tests
+   */
+  public function testPerson($name, $tests)
+  {
+    $person = new Person($name);
+    if(isset($tests['prefix']))
+    {
+      $this->assertEquals($tests['prefix'], $person->getPrefix());
+    }
+    if(isset($tests['firstName']))
+    {
+      $this->assertEquals($tests['firstName'], $person->getFirstName());
+    }
+    if(isset($tests['middleNames']))
+    {
+      $this->assertEquals($tests['middleNames'], $person->getMiddleNames());
+    }
+    if(isset($tests['lastNameCompound']))
+    {
+      $this->assertEquals($tests['lastNameCompound'], $person->getLastNameCompound());
+    }
+    if(isset($tests['lastName']))
+    {
+      $this->assertEquals($tests['lastName'], $person->getLastName());
+    }
+    if(isset($tests['suffix']))
+    {
+      $this->assertEquals($tests['suffix'], $person->getSuffix());
+    }
+    if(isset($tests['profession']))
+    {
+      $this->assertEquals($tests['profession'], $person->getProfession());
+    }
+    if(isset($tests['nickname']))
+    {
+      $this->assertEquals($tests['nickname'], $person->getNickname());
+    }
+    if(isset($tests['fullName']))
+    {
+      $this->assertEquals($tests['fullName'], $person->getFullName());
+    }
+  }
+
+  public function data()
+  {
+    return [
+      [
+        'Brian Senior',
+        [
+          'firstName' => 'Brian',
+          'lastName'  => 'Senior',
+          'fullName'  => 'Brian Senior',
+        ],
+      ],
+      [
+        'Brian Senior jr',
+        [
+          'firstName' => 'Brian',
+          'lastName'  => 'Senior',
+          'fullName'  => 'Brian Senior Junior',
+        ],
+      ],
+      [
+        'Brian Senior jr Sr',
+        [
+          'firstName'   => 'Brian',
+          'middleNames' => 'Senior',
+          'lastName'    => 'Junior',
+          'fullName'    => 'Brian Senior Junior Senior',
+        ],
+      ],
+      [
+        'edward lodewijk van halen',
+        [
+          'firstName'        => 'Edward',
+          'middleNames'      => 'Lodewijk',
+          'lastNameCompound' => 'Van',
+          'lastName'         => 'Van Halen',
+        ],
+      ],
+      [
+        'prof.kay, tom (Blacksmith) sr iii phd.bsc.m.b',
+        [
+          'prefix'     => ['Prof.'],
+          'nickname'   => 'Blacksmith',
+          'firstName'  => 'Tom',
+          'lastName'   => 'Kay',
+          'suffix'     => ['Senior', 'III'],
+          'profession' => ['PhD', 'BSc', 'MB'],
+        ],
+      ],
+      [
+        'prof. kay, tom (Blacksmith) sr iii phd. bsc. m.b.',
+        [
+          'prefix'     => ['Prof.'],
+          'nickname'   => 'Blacksmith',
+          'firstName'  => 'Tom',
+          'lastName'   => 'Kay',
+          'suffix'     => ['Senior', 'III'],
+          'profession' => ['PhD', 'BSc', 'MB'],
+        ],
+      ],
+      [
+        'brooke a j bryan',
+        [
+          'firstName'   => 'Brooke',
+          'lastName'    => 'Bryan',
+          'middleNames' => 'A J',
+          'fullName'    => 'Brooke A J Bryan',
+        ],
+      ],
+      [
+        'BROOKE A J BRYAN',
+        [
+          'firstName'   => 'Brooke',
+          'lastName'    => 'Bryan',
+          'middleNames' => 'A J',
+          'fullName'    => 'Brooke A J Bryan',
+        ],
+      ],
+    ];
+  }
+}

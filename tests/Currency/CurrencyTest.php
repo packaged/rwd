91b07893276f7704e7b94dbc8b36ab1035ec64ae
Tom Kay <oridan82@gmail.com>
2023-01-16 15:29:44 +0000

    delta check

--- tests/Currency/CurrencyTest.php
+++ tests/Currency/CurrencyTest.php
@@ -9,22 +9,22 @@ class CurrencyTest extends TestCase
   public function testUsdCurrencyValue()
   {
     $gbp = CurrencyHelper::getCurrency('GBP');
-    $this->assertEquals(1, $gbp->getUSDAverageValue($gbp->getUSDAverage()), '', 0.001);
-    self::assertEquals(1.658, $gbp->getUSDAverageValue(1), '', 0.001);
+    self::assertEqualsWithDelta(1, $gbp->getUSDAverageValue($gbp->getUSDAverage()), 0.001);
+    self::assertEqualsWithDelta(1.658, $gbp->getUSDAverageValue(1), 0.001);
 
     $jpy = CurrencyHelper::getCurrency('JPY');
-    $this->assertEquals(1, $jpy->getUSDAverageValue($jpy->getUSDAverage()), '', 0.001);
-    $this->assertEquals(1.994, $jpy->getUSDAverageValue(200), '', 0.001);
+    self::assertEqualsWithDelta(1, $jpy->getUSDAverageValue($jpy->getUSDAverage()), 0.001);
+    self::assertEqualsWithDelta(1.994, $jpy->getUSDAverageValue(200), 0.001);
   }
 
   public function testCurrencyFormat()
   {
     $gbp = CurrencyHelper::getCurrency('GBP');
-    $this->assertEquals('£123.00', $gbp->format(123));
-    $this->assertEquals('-£123.00', $gbp->format(-123));
+    self::assertEquals('£123.00', $gbp->format(123));
+    self::assertEquals('-£123.00', $gbp->format(-123));
 
     $jpy = CurrencyHelper::getCurrency('JPY');
-    $this->assertEquals('¥123', $jpy->format(123));
-    $this->assertEquals('-¥123', $jpy->format(-123));
+    self::assertEquals('¥123', $jpy->format(123));
+    self::assertEquals('-¥123', $jpy->format(-123));
   }
 }
